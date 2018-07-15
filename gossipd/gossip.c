54580469452842bc651e66d742f2b15a2a5a94ab
Christian Decker <decker.christian@gmail.com>
2018-07-15 22:40:38 +0200

    fixup! gossip: Assert that all three arrays in getnodes have the same count

--- gossipd/gossip.c
+++ gossipd/gossip.c
@@ -3575,9 +3575,9 @@ static struct io_plan *get_peers(struct io_conn *conn,
 		n++;
 	}
 
-	assert(tal_len(id) == n-1);
-	assert(tal_len(wireaddr) == n-1);
-	assert(tal_len(nodes) == n-1);
+	assert(tal_len(id) == n);
+	assert(tal_len(wireaddr) == n);
+	assert(tal_len(nodes) == n);
 
 	daemon_conn_send(&daemon->master,
 			 take(towire_gossip_getpeers_reply(NULL, id, wireaddr, nodes)));
